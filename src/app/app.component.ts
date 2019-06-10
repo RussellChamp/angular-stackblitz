1182addd8e07ee2780f2069870c71a55869f9de3
RussellChamp <russellchamp@gmail.com>
2019-06-10 16:04:59 -0400

    3 files modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { OnChange } from 'property-watch-decorator';
 
 @Component({
   selector: 'my-app',
@@ -6,5 +7,8 @@ import { Component } from '@angular/core';
   styleUrls: [ './app.component.css' ]
 })
 export class AppComponent  {
-  name = 'Angular';
+  @OnChange<string>((value) => {
+    console.log(value + '!!');
+  })
+  public name: string = 'Angular';
 }
